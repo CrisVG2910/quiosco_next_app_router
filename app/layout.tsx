f9792eeb446947c155e58ff33fc06fd01cd1bddd
Cristobal Vargas <cris2910.vg@gmail.com>
2024-04-04 22:53:14 -0300

    nombre2

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Quisco de Comida",
-  description: "Quisco de Comida",
+  title: "Quiosco de Comida",
+  description: "Quiosco de Comida",
 };
 
 export default function RootLayout({
