783b9ed5bbc7b01e058cf5e081136c79d427ef5c
Cristobal Vargas <cris2910.vg@gmail.com>
2024-04-04 22:14:09 -0300

    first commit

---
+++ components/order/OrderSidebar.tsx
@@ -0,0 +1,30 @@
+import { prisma } from '@/src/lib/prisma'
+import Categoryicon from '../ui/Categoryicon'
+import { categories } from '../../prisma/data/categories';
+import Logo from '../ui/Logo';
+
+async function getCategories() {
+  return await prisma.category.findMany()
+  
+}
+
+export default async function OrderSidebar() {
+  const categories = await getCategories()
+
+  console.log(categories)
+
+  return (
+    <aside className="md:w-72 md:h-screen bg-white">
+
+        <Logo />
+        <nav className='mt-10'>
+          {categories.map(category => (
+            <Categoryicon 
+              key={category.id}
+              category={category}
+            />
+          ))}
+        </nav>
+    </aside>
+  )
+}
